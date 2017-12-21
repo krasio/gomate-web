53834b2cf175fda37aa12f1297e75995812e40a8
Krasimir Angelov <krasio@codingspree.net>
2017-12-21 13:59:22 +1300

    implement graceful shutdown.

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/codegangsta/negroni"
 	"github.com/krasio/gomate-web/handlers"
 	"github.com/krasio/gomate-web/version"
+	"github.com/urfave/negroni"
 )
 
 func main() {
 	log.Printf(
-		"GOMATE - commit: %s, build time: %s, release: %s.",
+		"[GOMATE] commit: %s, build time: %s, release: %s.",
 		version.Commit, version.BuildTime, version.Release,
 	)
 
@@ -22,7 +26,30 @@ func main() {
 
 	router := handlers.Router()
 
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
 	n := negroni.Classic()
 	n.UseHandler(router)
-	n.Run(":" + port)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: n,
+	}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	killSignal := <-interrupt
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("[GOMATE] Got SIGINT.")
+	case syscall.SIGTERM:
+		log.Print("[GOMATE] Got SIGTERM.")
+	}
+
+	log.Print("[GOMATE] Shutting down...")
+	srv.Shutdown(context.Background())
+	log.Print("[GOMATE] Done.")
 }
