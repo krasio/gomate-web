51c6eea7c2d25958d7770f14c623a4ce78daae41
Krasimir Angelov <krasio@codingspree.net>
2017-12-21 13:05:49 +1300

    Add versioning.

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/codegangsta/negroni"
 	"github.com/krasio/gomate-web/handlers"
+	"github.com/krasio/gomate-web/version"
 )
 
 func main() {
+	log.Printf(
+		"GOMATE - commit: %s, build time: %s, release: %s.",
+		version.Commit, version.BuildTime, version.Release,
+	)
+
 	port, ok := os.LookupEnv("GOMATE_PORT")
 	if !ok {
 		port = "8080"
